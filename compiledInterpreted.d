6681302bc94ee6ddbc0af6375841945d0b41e676
Jacob Mennell <67950889+jacob-mennell@users.noreply.github.com>
2023-10-04 09:25:54 +0100

    Create compiledInterpreted.d

---
+++ compiledInterpreted.d
@@ -0,0 +1,14 @@
+**Compiled vs. Interpreted Languages**:
+
+1. **Compiled Language**:
+   - In a compiled language, the source code is translated into machine code or an intermediate code by a compiler before execution.
+   - Compilation occurs before the program is run, and the resulting binary code is executed directly by the computer's CPU.
+   - Compiled languages tend to offer better performance because the code is optimized before execution.
+   - Examples include C, C++, and Rust.
+
+2. **Interpreted Language**:
+   - In an interpreted language, the source code is not compiled into machine code. Instead, an interpreter reads and executes the code line by line at runtime.
+   - Interpreted languages are often more flexible and platform-independent but may have slower execution compared to compiled languages.
+   - Examples include Python, JavaScript, and Ruby.
+
+In summary, compiled languages are translated into machine code or an intermediate form before execution, offering performance benefits, while interpreted languages are executed line by line by an interpreter at runtime, providing flexibility and ease of development.
